20fc8102d54cd1252b05b7b85719ea10ee83a968
amsuarezp <amsuarezp@gmail.com>
2020-05-19 10:16:46 -0500

    create objects.py and squish.py

---
+++ config.py
@@ -0,0 +1,21 @@
+
+# Image Files
+banana_image = "banana.PNG"
+weight_image = "weight.PNG"
+splash_image = "weight.PNG"
+
+# Size of the screen
+screen_size = 800,600
+
+# White color for the background
+background_color = 255, 255, 255
+margin = 30
+full_screen = 48
+
+# These affect the behavior of the game
+drop_speed = 5
+banana_spreed = 10
+speed_increase = 1
+weights_per_level = 10
+banana_pad_top = 40
+banana_pad_side = 20
